91cb5e239a136867ed238a276d8599027898908f
eduard7mar <edwardmargaryan@gmail.com>
2024-01-30 17:25:23 +0200

    updated

--- codewars.js
+++ codewars.js
@@ -69,3 +69,20 @@ function monkeyCount(n) {
 function fixTheMeerkat(arr) {
   return arr.reverse();
 }
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+// Solution
+function powersOfTwo(n) {
+  let array = [];
+  let i = 0;
+  while (i <= n) {
+    array.push(Math.pow(2, i));
+    i++;
+  }
+  return array;
+}
