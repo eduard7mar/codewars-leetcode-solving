f116654a1a3a9368633a03c02fe2e0eb767b9a8f
eduard7mar <edwardmargaryan@gmail.com>
2024-01-31 21:28:12 +0200

    update

--- codewars.js
+++ codewars.js
@@ -139,5 +139,28 @@ function greet(name) {
   return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
 }
 
+// If you can't sleep, just count sheep!!
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// Solution
+var countSheep = function(num) {
+  if (num === 0) {
+    return "";
+  }
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    result.push(`${i} sheep...`);
+  }
+  return result.join("");
+}
+
+var countSheep = function (num){
+  let str = "";
+  for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+  return str;
+}
+
+
 
 
