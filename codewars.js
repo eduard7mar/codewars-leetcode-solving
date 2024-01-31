c07129aa29052dd04cb77674bb5acd5a2d4398be
eduard7mar <edwardmargaryan@gmail.com>
2024-01-31 21:26:55 +0200

    update

--- codewars.js
+++ codewars.js
@@ -120,3 +120,15 @@ function stringToArray(string){
 function hoopCount (n) {
    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";  
 }
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+// Solution
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ");
+}
+
+
