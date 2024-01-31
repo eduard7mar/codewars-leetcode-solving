61a0ac119d5ec8964afb806930f886a76b3845a8
eduard7mar <edwardmargaryan@gmail.com>
2024-01-31 21:26:05 +0200

    update

--- codewars.js
+++ codewars.js
@@ -110,3 +110,13 @@ function squareUp(n) {
 function stringToArray(string){
   return string.split(" ");
 }
+
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+// •	If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// •	If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+// Solution
+function hoopCount (n) {
+   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";  
+}
