3730aa660cc3f8407acfdb62828e6d7ac43e5e31
eduard7mar <edwardmargaryan@gmail.com>
2024-01-31 21:28:52 +0200

    update

--- codewars.js
+++ codewars.js
@@ -161,6 +161,31 @@ var countSheep = function (num){
   return str;
 }
 
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Examples input/output:
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+// Solution
+function XO(str) {
+    let array = str.toLowerCase().split("");
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].includes("x")) {
+        x++;
+      } 
+      if (array[i].includes("o")) {
+        y++;
+      }
+    }
+    return x === y ? true : false;
+}
+
+
 
 
 
