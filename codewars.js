abc99b93085251621c451ddaa91bd3d5b575762e
eduard7mar <edwardmargaryan@gmail.com>
2024-02-01 21:53:44 +0200

    update

--- codewars.js
+++ codewars.js
@@ -197,6 +197,44 @@ function findMultiples(integer, limit) {
   return result;
 }
 
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+// Solution
+function arrayPlusArray(arr1, arr2) {
+  return [...arr1, ...arr2].reduce((a, b) => a + b, 0);
+}
+
+// Our football team has finished the championship.
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Points are awarded for each match as follows:
+// •	if x > y: 3 points (win)
+// •	if x < y: 0 points (loss)
+// •	if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+// Notes:
+// •	our team always plays 10 matches in the championship
+// •	0 <= x <= 4
+// •	0 <= y <= 4
+
+// Solution
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
+
+
+
+
+
 
 
 
