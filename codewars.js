efedc568335227544416896ebeccad12ed9f90ba
eduard7mar <edwardmargaryan@gmail.com>
2024-02-06 22:14:03 +0200

    update

--- codewars.js
+++ codewars.js
@@ -349,3 +349,20 @@ function bonusTime(salary, bonus) {
 function enough(cap, on, wait) {
   return cap - on > wait ? 0 : -(cap - on - wait);
 }
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+// Solution
+function divisors(integer) {
+  const divisors = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      divisors.push(i);
+    }
+  }
+  return divisors.length > 0 ? divisors : `${integer} is prime`;
+};
