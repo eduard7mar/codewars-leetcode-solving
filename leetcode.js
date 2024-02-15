da2162dbcff9672121f7392dd2f4c893ef563169
eduard7mar <edwardmargaryan@gmail.com>
2024-02-15 20:37:49 +0200

    update

--- leetcode.js
+++ leetcode.js
@@ -54,6 +54,7 @@ var mostWordsFound = function (sentences) {
   return count;
 };
 
+
 // You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
 // You should rearrange the elements of nums such that the modified array follows the given conditions:
 // Every consecutive pair of integers have opposite signs.
@@ -83,3 +84,33 @@ var rearrangeArray = function(nums) {
   }
   return result;
 };
+
+
+// You are given an array of positive integers nums of length n.
+// A polygon is a closed plane figure that has at least 3 sides. The longest side of a polygon is smaller than the sum of its other sides.
+// Conversely, if you have k (k >= 3) positive real numbers a1, a2, a3, ..., ak where a1 <= a2 <= a3 <= ... <= ak and a1 + a2 + a3 + ... + ak-1 > ak, then there always exists a polygon with k sides whose lengths are a1, a2, a3, ..., ak.
+// The perimeter of a polygon is the sum of lengths of its sides.
+// Return the largest possible perimeter of a polygon whose sides can be formed from nums, or -1 if it is not possible to create a polygon.
+
+// Example 1:
+
+// Input: nums = [5,5,5]
+// Output: 15
+// Explanation: The only possible polygon that can be made from nums has 3 sides: 5, 5, and 5. The perimeter is 5 + 5 + 5 = 15.
+
+// Solution
+var largestPerimeter = function(nums) {
+  nums.sort((a,b)=>a-b); // Sorting the array in non-decreasing order
+  let maxPerimeter = -1; // Initializing maxPerimeter to a non-positive value
+  let curSum = 0; // Initializing the current sum to 0
+  
+  // Iterating through the sorted array
+  for(let i = 0; i < nums.length; i++) {
+      if(i >= 2 && curSum > nums[i]) { // Checking if a valid triplet is found
+          maxPerimeter = curSum + nums[i]; // Updating maxPerimeter
+      }
+      curSum += nums[i]; // Updating the current sum
+  }
+
+  return maxPerimeter; // Returning the maximum perimeter found
+}
