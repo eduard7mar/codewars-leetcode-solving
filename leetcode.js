e76f5326e5634cd8d07cba58693394306e1d37de
eduard7mar <edwardmargaryan@gmail.com>
2024-02-18 22:12:23 +0200

    update

--- leetcode.js
+++ leetcode.js
@@ -118,28 +118,28 @@ var largestPerimeter = function (nums) {
 // Output: 1
 // Explanation: Remove the single 4, only 5 is left.
 // Solution
-var findLeastNumOfUniqueInts = function(arr, k) {
+var findLeastNumOfUniqueInts = function (arr, k) {
   const frequency = {};
   for (const num of arr) {
-      frequency[num] = (frequency[num] || 0) + 1;
+    frequency[num] = (frequency[num] || 0) + 1;
   }
-  
+
   const uniqueNums = Object.keys(frequency).map(Number);
   uniqueNums.sort((a, b) => frequency[a] - frequency[b]);
-  
+
   let uniqueCount = uniqueNums.length;
   let removeCount = 0;
-  
+
   for (const num of uniqueNums) {
-      const frequencyOfNum = frequency[num];
-      if (k >= frequencyOfNum) {
-          k -= frequencyOfNum;
-          removeCount += 1;
-      } else {
-          break;
-      }
+    const frequencyOfNum = frequency[num];
+    if (k >= frequencyOfNum) {
+      k -= frequencyOfNum;
+      removeCount += 1;
+    } else {
+      break;
+    }
   }
-  
+
   return uniqueCount - removeCount;
 };
 
@@ -154,25 +154,80 @@ var furthestBuilding = function (heights, bricks, ladders) {
   const n = heights.length - 1;
   const maxPQ = new MaxPriorityQueue();
   for (let i = 0; i < n; i++) {
-      const curDiff = heights[i + 1] - heights[i];
-      if (curDiff > 0) {
-          if (bricks < curDiff) {
-              if (ladders === 0) return i;
-              ladders -= 1;
-              if (maxPQ.isEmpty()) continue;
-              const maxDiff = maxPQ.front().element;
-              if (maxDiff > curDiff) {
-                  maxPQ.dequeue();
-                  maxPQ.enqueue(curDiff);
-                  // we'll add maxDiff amount of bricks back to current bricks
-                  // we'll use curDiff amount of bricks from current bricks
-                  bricks += (maxDiff - curDiff);
-              }
-          } else {
-              bricks -= curDiff;
-              maxPQ.enqueue(curDiff);
-          }
+    const curDiff = heights[i + 1] - heights[i];
+    if (curDiff > 0) {
+      if (bricks < curDiff) {
+        if (ladders === 0) return i;
+        ladders -= 1;
+        if (maxPQ.isEmpty()) continue;
+        const maxDiff = maxPQ.front().element;
+        if (maxDiff > curDiff) {
+          maxPQ.dequeue();
+          maxPQ.enqueue(curDiff);
+          // we'll add maxDiff amount of bricks back to current bricks
+          // we'll use curDiff amount of bricks from current bricks
+          bricks += maxDiff - curDiff;
+        }
+      } else {
+        bricks -= curDiff;
+        maxPQ.enqueue(curDiff);
       }
+    }
   }
   return n;
+};
+
+// You are given an integer n. There are n rooms numbered from 0 to n - 1.
+// You are given a 2D integer array meetings where meetings[i] = [starti, endi] means that a meeting will be held during the half-closed time interval [starti, endi). All the values of starti are unique.
+// Meetings are allocated to rooms in the following manner:
+// Each meeting will take place in the unused room with the lowest number.
+// If there are no available rooms, the meeting will be delayed until a room becomes free. The delayed meeting should have the same duration as the original meeting.
+// When a room becomes unused, meetings that have an earlier original start time should be given the room.
+// Return the number of the room that held the most meetings. If there are multiple rooms, return the room with the lowest number.
+// A half-closed interval [a, b) is the interval between a and b including a and not including b.
+// Example 1:
+// Input: n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]
+// Output: 0
+// Explanation:
+// - At time 0, both rooms are not being used. The first meeting starts in room 0.
+// - At time 1, only room 1 is not being used. The second meeting starts in room 1.
+// - At time 2, both rooms are being used. The third meeting is delayed.
+// - At time 3, both rooms are being used. The fourth meeting is delayed.
+// - At time 5, the meeting in room 1 finishes. The third meeting starts in room 1 for the time period [5,10).
+// - At time 10, the meetings in both rooms finish. The fourth meeting starts in room 0 for the time period [10,11).
+// Both rooms 0 and 1 held 2 meetings, so we return 0.
+// Solution
+var mostBooked = function (n, meetings) {
+  const counts = new Array(n).fill(0);
+  // Store the end time of each room
+  const endings = new Array(n).fill(0);
+  // Sort by start time
+  meetings.sort((a, b) => a[0] - b[0]);
+
+  for (const [start, end] of meetings) {
+    // First, look for first room where meeting is compatible
+    let roomIdx = -1;
+    for (let i = 0; i < n; i++) {
+      if (start >= endings[i]) {
+        roomIdx = i;
+        break;
+      }
+    }
+    // If no room available, use earliest ending time instead
+    if (roomIdx === -1) {
+      roomIdx = 0;
+      for (let i = 0; i < n; i++) {
+        if (endings[i] < endings[roomIdx]) roomIdx = i;
+      }
+    }
+    counts[roomIdx]++;
+    // Update room end time, use difference when meeting is delayed
+    endings[roomIdx] =
+      endings[roomIdx] < start ? end : endings[roomIdx] + (end - start);
+  }
+  let maxIdx = 0;
+  for (let i = 0; i < counts.length; i++) {
+    if (counts[i] > counts[maxIdx]) maxIdx = i;
+  }
+  return maxIdx;
 };
