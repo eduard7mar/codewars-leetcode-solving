fb76e04747cb9a1d31aef2dcc03616771e796d76
eduard7mar <edwardmargaryan@gmail.com>
2024-02-20 20:26:22 +0200

    update

--- leetcode.js
+++ leetcode.js
@@ -246,3 +246,20 @@ var mostBooked = function (n, meetings) {
 var isPowerOfTwo = function (n) {
   return n > 0 && (n & (n - 1)) === 0;
 };
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+// Example 1:
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+// Example 2:
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+// Solution
+var missingNumber = function(nums) {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+  return expectedSum - actualSum;
+};
