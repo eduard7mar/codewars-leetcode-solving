8b363803bbaefa5608d3aff833823dfb1bb6dba5
eduard7mar <edwardmargaryan@gmail.com>
2024-02-24 22:11:47 +0200

    update

--- leetcode.js
+++ leetcode.js
@@ -322,3 +322,60 @@ function findCheapestPrice(n, flights, src, dst, k) {
 
   return prices[dst] === Infinity ? -1 : prices[dst];
 }
+
+// You are given an integer n indicating there are n people numbered from 0 to n - 1. You are also given a 0-indexed 2D integer array meetings where meetings[i] = [xi, yi, timei] indicates that person xi and person yi have a meeting at timei. A person may attend multiple meetings at the same time. Finally, you are given an integer firstPerson.
+// Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
+// The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
+// Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.
+// Example 1:
+// Input: n = 6, meetings = [[1,2,5],[2,3,8],[1,5,10]], firstPerson = 1
+// Output: [0,1,2,3,5]
+// Explanation:
+// At time 0, person 0 shares the secret with person 1.
+// At time 5, person 1 shares the secret with person 2.
+// At time 8, person 2 shares the secret with person 3.
+// At time 10, person 1 shares the secret with person 5.​​​​
+// Thus, people 0, 1, 2, 3, and 5 know the secret after all the meetings.
+// Solution
+var findAllPeople = function (n, meetings, firstPerson) {
+  const find = (groups, index) => {
+    while (index !== groups[index]) {
+      index = groups[index];
+    }
+    return index;
+  };
+
+  let groups = new Array(100000).fill(0).map((_, i) => i);
+  let result = [];
+
+  groups[firstPerson] = 0;
+
+  meetings.sort((a, b) => a[2] - b[2]);
+
+  let i = 0;
+  while (i < meetings.length) {
+    let currentTime = meetings[i][2];
+    let temp = [];
+    while (i < meetings.length && meetings[i][2] === currentTime) {
+      let g1 = find(groups, meetings[i][0]);
+      let g2 = find(groups, meetings[i][1]);
+      groups[Math.max(g1, g2)] = Math.min(g1, g2);
+      temp.push(meetings[i][0]);
+      temp.push(meetings[i][1]);
+      ++i;
+    }
+    for (let j = 0; j < temp.length; ++j) {
+      if (find(groups, temp[j]) !== 0) {
+        groups[temp[j]] = temp[j];
+      }
+    }
+  }
+
+  for (let j = 0; j < n; ++j) {
+    if (find(groups, j) === 0) {
+      result.push(j);
+    }
+  }
+
+  return result;
+};
