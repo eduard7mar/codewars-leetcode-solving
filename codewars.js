1fc26c4230bce78c7a0be552de150eb7cb5e94e1
eduard7mar <edwardmargaryan@gmail.com>
2024-03-01 12:03:19 +0200

    update

--- codewars.js
+++ codewars.js
@@ -541,3 +541,31 @@ function switchItUp(number){
             return "Number out of range";
     }
 }
+
+// I have a cat and a dog.
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+// Return their respective ages now as [humanYears,catYears,dogYears]
+// NOTES:
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Solution
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears, dogYears;
+  
+  if (humanYears === 1) {
+      catYears = 15;
+      dogYears = 15;
+  } else if (humanYears === 2) {
+      catYears = 24;
+      dogYears = 24;
+  } else {
+      catYears = 24 + 4 * (humanYears - 2);
+      dogYears = 24 + 5 * (humanYears - 2);
+  }
+  
+  return [humanYears, catYears, dogYears];
+}
