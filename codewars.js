ecd93dca6006515f0e2deb7c024cd5bd63b5d03e
eduard7mar <edwardmargaryan@gmail.com>
2024-03-04 21:21:42 +0200

    update

--- codewars.js
+++ codewars.js
@@ -652,7 +652,7 @@ function multiply(number) {
 // "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
 // "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
 
-// Solution
+// Solutions
 String.prototype.toAlternatingCase = function () {
   return this.split('').map(function(char) {
         if (char >= 'a' && char <= 'z') {
@@ -668,4 +668,17 @@ String.prototype.toAlternatingCase = function () {
 String.prototype.toAlternatingCase = function () {
     return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
 }
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+// Example: (input --> output)
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+// Solution
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]);
+
 
