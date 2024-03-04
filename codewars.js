1199afd0274b56dd11a1b356bc010e4bc45e29eb
eduard7mar <edwardmargaryan@gmail.com>
2024-03-04 21:20:49 +0200

    update

--- codewars.js
+++ codewars.js
@@ -643,3 +643,29 @@ function multiply(number){
 function multiply(number) {
   return number * Math.pow(5, Math.abs(number).toString().length);
 }
+
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+
+// Solution
+String.prototype.toAlternatingCase = function () {
+  return this.split('').map(function(char) {
+        if (char >= 'a' && char <= 'z') {
+            return char.toUpperCase();
+        } else if (char >= 'A' && char <= 'Z') {
+            return char.toLowerCase();
+        } else {
+            return char;
+        }
+    }).join('');
+}
+
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+}
+
