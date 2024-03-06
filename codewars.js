bc27f304d547d63267d113f0324e4dc49cd2e35e
eduard7mar <edwardmargaryan@gmail.com>
2024-03-06 21:59:43 +0200

    update

--- codewars.js
+++ codewars.js
@@ -766,3 +766,15 @@ function solution(number) {
   }
   return sum;
 }
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+// [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+// Solution
+function findOutlier(integers){
+  const evenNumbers = integers.filter(item => item % 2 === 0);
+  const oddNumbers = integers.filter(item => item % 2 !== 0);
+  return evenNumbers.length === 1 ? evenNumbers[0] : oddNumbers[0];
+}
+
