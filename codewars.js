21994b242ca66e61bb0823bbaeed7508eb814bcc
eduard7mar <edwardmargaryan@gmail.com>
2024-03-11 20:43:34 +0200

    update

--- codewars.js
+++ codewars.js
@@ -843,3 +843,30 @@ function arithmetic(a, b, operator){
   }
 }
 
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+// Examples:
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+// You can assume that all inputs are valid integers.
+// Solution
+function roundToNext5(n){
+  // If n is negative and not a multiple of 5, round up towards 0
+  if (n < 0 && n % 5 !== 0) {
+    return n - (n % 5);
+  }
+  // Otherwise, round up to the next multiple of 5
+  return Math.ceil(n / 5) * 5;
+}
+
+function roundToNext5(n){
+  return Math.ceil(n/5)*5;
+}
