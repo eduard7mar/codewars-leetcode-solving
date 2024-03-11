6f7f95734b594972639d5af8791587152ad42ff1
eduard7mar <edwardmargaryan@gmail.com>
2024-03-11 20:39:45 +0200

    11.03.2024

--- codewars.js
+++ codewars.js
@@ -772,9 +772,27 @@ function solution(number) {
 // [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
 // [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
 // Solution
-function findOutlier(integers){
-  const evenNumbers = integers.filter(item => item % 2 === 0);
-  const oddNumbers = integers.filter(item => item % 2 !== 0);
+function findOutlier(integers) {
+  const evenNumbers = integers.filter((item) => item % 2 === 0);
+  const oddNumbers = integers.filter((item) => item % 2 !== 0);
   return evenNumbers.length === 1 ? evenNumbers[0] : oddNumbers[0];
 }
 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+// Solution
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
