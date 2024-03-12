747b1e5117b719b0bdf69e71e26d206335abef7e
eduard7mar <edwardmargaryan@gmail.com>
2024-03-12 16:11:09 +0200

    update

--- codewars.js
+++ codewars.js
@@ -896,6 +896,19 @@ const sequenceSum = (begin, end, step) => {
 // solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 // solution(null); // should return []
 // Solution
-function solution(nums){
-  return (nums === null || nums.length === 0) ? [] : nums.sort((a, b) => a - b);
+function solution(nums) {
+  return nums === null || nums.length === 0 ? [] : nums.sort((a, b) => a - b);
+}
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+// Solution
+function removeUrlAnchor(url) {
+  return url.split("#")[0];
+}
+
+function removeUrlAnchor(url){
+  return url.replace(/#.*/gi,"");
 }
