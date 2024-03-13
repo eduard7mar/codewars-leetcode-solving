b0de821e0d40e394314d7f929179488a98af5335
eduard7mar <edwardmargaryan@gmail.com>
2024-03-13 13:29:37 +0200

    update

--- codewars.js
+++ codewars.js
@@ -947,3 +947,15 @@ var capitals = function (word) {
 function smallEnough(a, limit){
   return a.filter(item => item > limit).length > 0 ? false : true;
 }
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+// For example: (Input --> Output)
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+// Solution
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+}
+
