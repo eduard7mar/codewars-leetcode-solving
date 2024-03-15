2169e1451bb41fbd25e8efbe87f9f11486186ea9
eduard7mar <edwardmargaryan@gmail.com>
2024-03-15 12:48:19 +0200

    update

--- codewars.js
+++ codewars.js
@@ -1061,3 +1061,14 @@ function flattenAndSort(array) {
   return [].concat(...array).sort((a,b) => a - b);
 }
 
+// Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+// For Example:
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+// Solution
+function sumOfMinimums(arr) {
+  return arr.map(item => Math.min(...item)).reduce((a, b) => a + b);
+}
