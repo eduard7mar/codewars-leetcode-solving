101344fb81fe89ec3211fdd990df96a91d5e1714
eduard7mar <edwardmargaryan@gmail.com>
2024-03-21 20:19:34 +0200

    update

--- codewars.js
+++ codewars.js
@@ -1107,3 +1107,26 @@ function factorial(n) {
   }
   return result;
 }
+
+function factorial(n) {
+  if (n < 0 || n > 12)
+    throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
+
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+// Your task is to return the number of JavaScript developers coming from Europe.
+// For example, given the following list:
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+// If, there are no JavaScript developers from Europe then your function should return 0.
+// Solution
+function countDevelopers(list) {
+  let newArray = list.filter(item => item.continent === 'Europe' && item.language === 'JavaScript');
+  return newArray.length > 0 ? newArray.length : 0;
+}
