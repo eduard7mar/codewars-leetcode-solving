2c110846d4a4d337ee12e7ff5bc2ebb1024c2b27
eduard7mar <edwardmargaryan@gmail.com>
2024-03-21 20:16:16 +0200

    21.03.2024

--- codewars.js
+++ codewars.js
@@ -1089,6 +1089,21 @@ function sumOfMinimums(arr) {
 // If either input is an empty string, consider it as zero.
 // Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
 // Solution
-function sumStr(a,b) {
+function sumStr(a, b) {
   return (+a + +b).toString();
+}
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+// More details about factorial can be found here.
+// Solution
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError("The value should be in the range of 0 to 12.");
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
