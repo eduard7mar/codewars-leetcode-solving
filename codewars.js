704caa6fd203fe665c94be4265d7535b0eacef96
eduard7mar <edwardmargaryan@gmail.com>
2024-04-02 22:37:30 +0300

    02.04.2024

--- codewars.js
+++ codewars.js
@@ -1262,3 +1262,14 @@ function adjacentElementsProduct(array) {
   }
   return Math.max(...newArray)
 }
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+// Example:
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+// Solution
+var greet = function(name) {
+  return `Hello ${name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase()}!`
+};
+
+
