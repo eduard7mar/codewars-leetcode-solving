c9a506f6aaab9e2bdf2046178122c8e8c3c7e59f
eduard7mar <edwardmargaryan@gmail.com>
2024-04-09 20:22:52 +0300

    09.04.2024

--- codewars.js
+++ codewars.js
@@ -1384,3 +1384,19 @@ function sumCubes(n) {
 function isPowerOfTwo(n) {
   return Number.isInteger(Math.log2(n));
 }
+
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+// +++
+// +++
+// +++
+// Solution
+function generateShape(n) {
+  let row = "+".repeat(n);
+  let square = [];
+  for (let i = 0; i < n; i++) {
+    square.push(row);
+  }
+  return square.join("\n");
+}
