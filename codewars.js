610632f1bc10a8e844c3a315138a250ffd142ae3
eduard7mar <edwardmargaryan@gmail.com>
2024-04-22 14:34:06 +0300

    22.04.2024

--- codewars.js
+++ codewars.js
@@ -1438,3 +1438,26 @@ function repeats(arr){
   return arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v)).reduce((a,b) => a + b, 0);
 };
 
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+// For example:
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like 
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+// Solution
+function add(arr) {
+  let sum = 0;
+  return arr.map((num) => {
+    sum += num;
+    return sum;
+  });
+}
+
+function add(arr) {
+  let sum = arr[0];
+  const result = [];
+  for(let i = 1; i <= arr.length; i++) {
+    result.push(sum);
+    sum += arr[i];
+  }
+  return result;
+}
+
