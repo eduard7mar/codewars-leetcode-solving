5305c23cf702c3f6e65c0c0b8710ff91b2ecbf78
eduard7mar <edwardmargaryan@gmail.com>
2024-04-23 16:14:13 +0300

    update

--- codewars.js
+++ codewars.js
@@ -1516,3 +1516,7 @@ const greetDevelopers = list => list.map(
   dev => ({...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`})
 );
 
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+// Solution
+let hasUniqueChars = (str) => new Set(str).size === str.length;
