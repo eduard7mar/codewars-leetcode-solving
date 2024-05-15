5e8c6126864866fb8b5f8385a82feb17e8676309
eduard7mar <edwardmargaryan@gmail.com>
2024-05-15 13:20:06 +0300

    15.05.2024

--- codewars.js
+++ codewars.js
@@ -1724,10 +1724,42 @@ function spacify(str) {
 // Task
 // Given a number determine if it Automorphic or not.
 // Solution
-function automorphic(n){
+function automorphic(n) {
   let numberLength = n.toString().split("").length;
-  let firstLastNumber = +Math.pow(n, 2).toString().split("").splice(-numberLength).join("");
+  let firstLastNumber = +Math.pow(n, 2)
+    .toString()
+    .split("")
+    .splice(-numberLength)
+    .join("");
   return firstLastNumber === n ? "Automorphic" : "Not!!";
 }
 
-const automorphic = n => String(n*n).endsWith(String(n)) ? "Automorphic" : "Not!!";
+const automorphic = (n) =>
+  String(n * n).endsWith(String(n)) ? "Automorphic" : "Not!!";
+
+// Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+// Task
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+// Examples
+// 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+// 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+// 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+// 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+// Solution
+function strong(n) {
+  let arr = String(n).split("");
+  let sum = 0;
+  function factorial(fnNum) {
+    if (fnNum <= 1) {
+      return 1;
+    } else {
+      return fnNum * factorial(fnNum - 1);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += factorial(arr[i]);
+  }
+  return n == sum ? "STRONG!!!!" : "Not Strong !!";
+}
