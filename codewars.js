5b3e35273274db99c9efaa83598715dc3bb92fe7
eduard7mar <edwardmargaryan@gmail.com>
2024-05-17 14:22:58 +0300

    17.05.2024

--- codewars.js
+++ codewars.js
@@ -1837,6 +1837,7 @@ function maxTriSum(numbers) {
 // sum of all digits to the left of the middle digit(s) -> 10
 // sum of all digits to the right of the middle digit(s) -> 20
 // 10 and 20 are not equal, so it's not balanced.
+// Solution
 function balancedNum(number) {
   const numStr = number.toString();
   const len = numStr.length;
@@ -1857,4 +1858,19 @@ function balancedNum(number) {
     }
   }
   return leftSum === rightSum ? "Balanced" : "Not Balanced";
+}
+
+// Task
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+// Note: the scores will always be unique (so no duplicate values)
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+// Solution
+function myLanguages(results) {
+  return Object.entries(results)
+    .filter(([language, score]) => score >= 60)
+    .sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
+    .map(([language]) => language);
 }
