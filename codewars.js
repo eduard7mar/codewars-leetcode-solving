3f7dac421c0489a73d73dc09a99b0bbdd52c3a49
eduard7mar <edwardmargaryan@gmail.com>
2024-05-27 10:19:44 +0300

    update

--- codewars.js
+++ codewars.js
@@ -1964,4 +1964,8 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
   return days;
 }
 
-
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+// Solution
+function alternateCase(s) {
+  return s.split("").map(item => item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()).join("");
+}
