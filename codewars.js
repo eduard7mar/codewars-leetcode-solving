9f818c80878716ec01f6dbb309f41c30ab4ff8fd
eduard7mar <edwardmargaryan@gmail.com>
2024-05-28 21:18:42 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2032,3 +2032,21 @@ function menFromBoys(arr){
   let odd = newArr.filter(item => item % 2 !== 0).sort((a, b) => b - a);
   return [...even, ...odd];
 }
+
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] 
+//the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+// Notes
+// •	If the array is null/nil/None or empty you should return empty array ([]).
+// •	The rotten fruit name will be in this camelcase (rottenFruit).
+// •	The returned array should be in lowercase.
+// Solution
+function removeRotten(bagOfFruits) {
+  if (bagOfFruits === null || bagOfFruits === undefined) {
+    return []
+  };
+  let result = bagOfFruits.map(item => item.startsWith("rotten") ? item.replace("rotten", "").toLowerCase() : item);
+  return result;
+}
+
+
+
