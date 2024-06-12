68bf89a2ebf1f01433a8f48bc46468f498672195
eduard7mar <edwardmargaryan@gmail.com>
2024-06-12 16:59:32 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2141,3 +2141,17 @@ function pairs(ar) {
   }
   return count;
 }
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+// All inputs will be valid.
+// Solution
+function last(x) {
+  return x.split(" ").sort((a, b) => {
+    const lastCharA = a[a.length - 1];
+    const lastCharB = b[b.length - 1];
+    if (lastCharA < lastCharB) return -1;
+    if (lastCharA > lastCharB) return 1;
+    return 0;
+  });
+}
