ce2b9fd4d6b8f6091219034bf7e13f9ede23137f
Eduard <eduard.marharian@gmail.com>
2024-06-26 12:42:22 +0300

    update

--- leetcode.js
+++ leetcode.js
@@ -539,3 +539,17 @@ var getCommon = function (nums1, nums2) {
 
   return common !== Infinity ? common : -1;
 };
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+function squares(x, n) {
+  if (n <= 0) return [];
+  
+  let result = [x];
+  for (let i = 1; i < n; i++) {
+    result.push((result[i - 1] ** 2))
+  }
+  return result;
+}
