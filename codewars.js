4319fd8b3e198158bb04b34d1cc90c6a44b173bf
Eduard <eduard.marharian@gmail.com>
2024-07-22 15:23:06 +0300

    22.07.2024

--- codewars.js
+++ codewars.js
@@ -2161,6 +2161,22 @@ function last(x) {
 // "312" should return "333122"
 // "102269" should return "12222666666999999999"
 function explode(s) {
-  return s.split("").map(item => item.repeat(item)).join("");
+  return s
+    .split("")
+    .map((item) => item.repeat(item))
+    .join("");
 }
 
+// Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+// Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+// Notes:
+// •	return an empty array if your array is empty
+// •	arrays will only contain numbers so don't worry about checking that
+// Examples:
+// [1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+// [4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+//                                        #  9+3 = 12  -->  2
+// Solution
+function incrementer(num) {
+  return num.map((a, i) => (a + i + 1) % 10);
+}
