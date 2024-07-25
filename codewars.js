4cf239df928a4e5d1aeb652b232b7a424ff29d2e
Eduard <eduard.marharian@gmail.com>
2024-07-25 16:04:25 +0300

    25.07.2024

--- codewars.js
+++ codewars.js
@@ -2180,3 +2180,36 @@ function explode(s) {
 function incrementer(num) {
   return num.map((a, i) => (a + i + 1) % 10);
 }
+
+// Your colleagues have been looking over you shoulder. When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
+// In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+// Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+// Happiness rating will be total score / number of people in the room.
+// Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+// Solution
+function outed(meet, boss) {
+  let sum = 0;
+  for (let key in meet) {
+    if (meet.hasOwnProperty(key)) {
+      sum += meet[key];
+    }
+  }
+  sum += meet[boss];
+  console.log(sum);
+  const keys = Object.keys(meet);
+  const numberOfKeys = keys.length;
+
+  const result = sum / numberOfKeys;
+
+  if (result > 5) {
+    return "Nice Work Champ!";
+  } else {
+    return "Get Out Now!";
+  }
+}
+
+function outed(meet, boss) {
+  let names = Object.keys(meet);
+  let score = names.reduce((s, v) => s + meet[v], 0) + meet[boss];
+  return score / names.length > 5 ? "Nice Work Champ!" : "Get Out Now!";
+}
