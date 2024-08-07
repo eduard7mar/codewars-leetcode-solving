3d6724d72ebfbc07816e7900236a6ecde930c08c
Eduard <eduard.marharian@gmail.com>
2024-08-07 21:11:22 +0300

    07.08.2024

--- codewars.js
+++ codewars.js
@@ -2251,10 +2251,33 @@ function maxProduct(numbers, size) {
 // remove_vowels("drake") // => "drk"
 // remove_vowels("aeiou") // => ""
 // Solution
-var removeVowels = function(str) {
-  const pattern = /[aeiou]/gi; 
- let result = str.split("").map(item => item.match(pattern) ? "" : item).join("");
- return result;
+var removeVowels = function (str) {
+  const pattern = /[aeiou]/gi;
+  let result = str
+    .split("")
+    .map((item) => (item.match(pattern) ? "" : item))
+    .join("");
+  return result;
+};
+
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+// Find the number of Friday 13th in the given year.
+// Input: Year in Gregorian calendar as integer.
+// Output: Number of Black Fridays in the year as an integer.
+// Examples:
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+// Solution
+function unluckyDays(year) {
+  let counter = 0;
+
+  for (let month = 0; month < 12; month++) {
+    let date = new Date(year, month, 13);
+
+    if (date.getDay() === 5) {
+      counter++;
+    }
+  }
+
+  return counter;
 }
-
-
