ced124d6dc5fdab580f32f47fb139bf724314465
Eduard <eduard.marharian@gmail.com>
2024-08-08 22:06:09 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2331,3 +2331,37 @@ function last(arr) {
 function meeting(x) {
   return x.includes("O") ? x.indexOf("O") : "None available!";
 }
+
+// Task
+// Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+// Notes
+// •	Array/list size is at least 3 .
+// •	Array/list's numbers Will be mixture of positives and negatives also zeros_
+// •	Repetition of numbers in the array/list could occur.
+// •	The Maximum Gap is computed Regardless the sign.
+// Input >> Output Examples
+// maxGap ({13,10,5,2,9}) ==> return (4)
+// Explanation:
+// •	The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+// maxGap ({-3,-27,-4,-2}) ==> return (23)
+// Explanation:
+// •	The Maximum Gap after sorting the array is 23 , The difference between  |-4- (-27) | = 23 .
+// •	Note : Regardless the sign of negativity .
+// maxGap ({-7,-42,-809,-14,-12}) ==> return (767)
+// Explanation:
+// •	The Maximum Gap after sorting the array is 767 , The difference between  | -809- (-42) | = 767 .
+// •	Note : Regardless the sign of negativity .
+// maxGap ({-54,37,0,64,640,0,-15}) //return (576)
+// Explanation:
+// •	The Maximum Gap after sorting the array is 576 , The difference between  | 64 - 640 | = 576 .
+// •	Note : Regardless the sign of negativity.
+// Solution
+function maxGap(numbers) {
+  let sort = numbers.sort((a, b) => a - b);
+  let diff = [];
+  for (let i = 0; i < sort.length - 1; i++) {
+    diff.push(sort[i] - sort[i + 1]);
+  }
+  let result = diff.map((item) => Math.abs(item)).sort((a, b) => a - b);
+  return result[result.length - 1];
+}
