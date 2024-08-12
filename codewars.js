8ca8dc4049b18c54ebbb620f276e64116b9d86cf
Eduard <eduard.marharian@gmail.com>
2024-08-12 20:23:00 +0300

    12.08.2024

--- codewars.js
+++ codewars.js
@@ -2432,3 +2432,18 @@ function minimumSteps(numbers, value) {
     }
   }
 }
+
+// The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+// Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+// Given a string of binary, return the version the photocopier gives you as a string.
+// Solution
+function broken(x) {
+  return x
+    .split("")
+    .map((item) => (item === "0" ? "1" : "0"))
+    .join("");
+}
+
+function broken(x) {
+  return x.replace(/./g, (char) => (char === "1" ? "0" : "1"));
+}
