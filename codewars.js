612aef40ebd3f1444f1caf9089f6fb73fdd0b1c8
Eduard <eduard.marharian@gmail.com>
2024-08-16 12:13:18 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2516,3 +2516,22 @@ function containAllRots(strng, arr) {
   }
   return true;
 }
+
+// The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+// Example:
+// Split the below string into other strings of size #3
+// 'supercalifragilisticexpialidocious'
+// Will return a new string
+// 'sup erc ali fra gil ist ice xpi ali doc iou s'
+// Assumptions:
+// String length is always greater than 0
+// String has no spaces
+// Size is always positive
+// Solution
+var splitInParts = function (s, partLength) {
+  let result = [];
+  for (let i = 0; i < s.length; i += partLength) {
+    result.push(s.slice(i, i + partLength));
+  }
+  return result.join(" ");
+};
