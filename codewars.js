e06b450f666f69dea934ae625537ef267ec70972
Eduard <eduard.marharian@gmail.com>
2024-08-16 12:11:50 +0300

    16.08.2024

--- codewars.js
+++ codewars.js
@@ -2489,3 +2489,30 @@ function tripleX(str) {
 }
 
 const tripleX = (str) => /^[^x]*xxx/.test(str);
+
+// Input:
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+// Note:
+// Though not correct in a mathematical sense
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
+// Solution
+function containAllRots(strng, arr) {
+  if (strng === "") return true;
+  for (let i = 0; i < strng.length; i++) {
+    let rotation = strng.slice(i) + strng.slice(0, i);
+    if (!arr.includes(rotation)) {
+      return false;
+    }
+  }
+  return true;
+}
