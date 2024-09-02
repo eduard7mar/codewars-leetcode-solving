cc3f5720d2574fdf3bcf297b230741d28c9575c8
Eduard <eduard.marharian@gmail.com>
2024-09-02 20:02:27 +0300

    02.09.2024

--- codewars.js
+++ codewars.js
@@ -2556,3 +2556,24 @@ function initializeNames(name) {
 
   return array.join(" ");
 }
+
+// In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+// For example:
+// solve("xyab","xzca") = "ybzc"
+// --The first string has 'yb' which is not in the second string.
+// --The second string has 'zc' which is not in the first string.
+// Notice also that you return the characters from the first string concatenated with those from the second string.
+// More examples in the tests cases.
+// Solution
+function solve(a, b) {
+  let uniqueInA = a
+    .split("")
+    .filter((char) => !b.includes(char))
+    .join("");
+  let uniqueInB = b
+    .split("")
+    .filter((char) => !a.includes(char))
+    .join("");
+
+  return uniqueInA + uniqueInB;
+}
