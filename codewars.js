a6f8b5f3af7444dfe9d28d9709408557e70bb7c3
Eduard <eduard.marharian@gmail.com>
2024-09-05 23:08:22 +0300

    05.09.2024

--- codewars.js
+++ codewars.js
@@ -2577,3 +2577,36 @@ function solve(a, b) {
 
   return uniqueInA + uniqueInB;
 }
+
+// Nickname Generator
+// Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+// If the 3rd letter is a consonant, return the first 3 letters.
+// nickname("Robert") //=> "Rob"
+// nickname("Kimberly") //=> "Kim"
+// nickname("Samantha") //=> "Sam"
+// If the 3rd letter is a vowel, return the first 4 letters.
+// nickname("Jeannie") //=> "Jean"
+// nickname("Douglas") //=> "Doug"
+// nickname("Gregory") //=> "Greg"
+// If the string is less than 4 characters, return "Error: Name too short".
+// Notes:
+// •	Vowels are "aeiou", so discount the letter "y".
+// •	Input will always be a string.
+// •	Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+// •	The input can be modified
+// Solution
+function nicknameGenerator(name) {
+  if (name.length < 4) {
+    return "Error: Name too short";
+  } else if (
+    name[2] === "a" ||
+    name[2] === "e" ||
+    name[2] === "i" ||
+    name[2] === "o" ||
+    name[2] === "u"
+  ) {
+    return name.substr(0, 4);
+  } else {
+    return name.substr(0, 3);
+  }
+}
