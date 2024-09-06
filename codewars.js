e3d092dacfa89ad7087c0de9c519da6e97b5c3ae
Eduard <eduard.marharian@gmail.com>
2024-09-06 22:00:39 +0300

    06.09.2024

--- codewars.js
+++ codewars.js
@@ -2610,3 +2610,22 @@ function nicknameGenerator(name) {
     return name.substr(0, 3);
   }
 }
+
+// Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3.
+// Note that the number will always be non-negative (>= 0).
+// Solution
+function insertDash(num) {
+  let str = num.toString();
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i];
+    if (i < str.length - 1 && str[i] % 2 !== 0 && str[i + 1] % 2 !== 0) {
+      result += "-";
+    }
+  }
+  return result;
+}
+
+function insertDash(num) {
+  return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+}
