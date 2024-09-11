9e9777d51a6ef46eff9a32ae2ea7ebf1c077b607
Eduard <eduard.marharian@gmail.com>
2024-09-11 12:59:06 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2764,3 +2764,20 @@ function getStrings(city) {
 function vowelOne(s) {
   return s.replace(/[^aeiou]/gi, "0").replace(/[aeiou]/gi, "1");
 }
+
+// Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+// Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+// Note that numbers written as strings are strings and must be sorted with the other strings.
+// Solution
+function dbSort(a) {
+  let numbers = [];
+  let strings = [];
+  a.forEach((item) =>
+    Number.isInteger(item) ? numbers.push(item) : strings.push(item)
+  );
+
+  numbers.sort((a, b) => a - b);
+  strings.sort();
+
+  return numbers.concat(strings);
+}
