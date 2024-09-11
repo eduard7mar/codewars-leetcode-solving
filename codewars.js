819dedb48de394ae65be7f73575e6808553e1630
Eduard <eduard.marharian@gmail.com>
2024-09-11 12:58:15 +0300

    11.09.2024

--- codewars.js
+++ codewars.js
@@ -2753,3 +2753,14 @@ function getStrings(city) {
     )
     .join(",");
 }
+
+// vowelOne
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+// Examples:
+// vowelOne( "abceios" ) // "1001110"
+// vowelOne( "aeiou, abc" ) // "1111100100"
+// Solution
+function vowelOne(s) {
+  return s.replace(/[^aeiou]/gi, "0").replace(/[aeiou]/gi, "1");
+}
