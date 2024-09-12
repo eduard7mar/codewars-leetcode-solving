34b766cf840db1b2ee45ff5418f2e1bfc0bbb54f
Eduard <eduard.marharian@gmail.com>
2024-09-12 19:04:25 +0300

    update

--- codewars.js
+++ codewars.js
@@ -2800,3 +2800,17 @@ var makeBackronym = function (string) {
     .map((char) => dict[char])
     .join(" ");
 };
+
+// The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+// In this kata we will recreate that stunt... sort of.
+// You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+// The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+// Return the resulting lamps string. See example tests for more clarity.
+// Solution
+function flyBy(lamps, drone) {
+  const dronePathLength = drone.length;
+  return lamps
+    .split("")
+    .map((lamp, index) => (index < dronePathLength ? "o" : "x"))
+    .join("");
+}
