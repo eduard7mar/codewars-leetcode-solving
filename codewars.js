e3493d60d41b9e467e7b47d018df3cfbea38d089
Eduard <eduard.marharian@gmail.com>
2024-10-22 21:08:34 +0300

    22.10.2024

--- codewars.js
+++ codewars.js
@@ -2884,3 +2884,18 @@ function multiples(m, n) {
   }
   return result;
 }
+
+
+// You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+// C.....m returns 'Escaped!' <-- more than three characters between
+// C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+// Solution
+function catMouse(x) {
+  return x.split("").filter(item => item === ".").length > 3 ? "Escaped!" : "Caught!";
+}
+
+function catMouse(x) {
+  return x.length <= 5 ? 'Caught!' : 'Escaped!';
+}
+
