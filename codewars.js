8e4e89c0daa707c0adee9f7fbbdd09c7263bd354
Eduard <eduard.marharian@gmail.com>
2024-10-25 21:21:49 +0300

    25.10.2024

--- codewars.js
+++ codewars.js
@@ -2942,3 +2942,18 @@ function stray(numbers) {
 }
 
 
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+// If no occurrences can be found, a count of 0 should be returned.
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); returns 1
+// str_count("Hello", 'l'); returns 2
+// str_count("", 'z'); returns 0
+
+// Solution
+function strCount(str, letter) {
+  let number = 0;
+  str.split("").map((item) => (item === letter ? number++ : number));
+  return number;
+}
