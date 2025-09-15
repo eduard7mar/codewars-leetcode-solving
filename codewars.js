b974c0f177d8e1c92ea1411a1afce1b9fcf85a2f
Eduard <eduard.marharian@gmail.com>
2025-09-15 17:40:49 +0300

    Remove First and Last Character Part Two

--- codewars.js
+++ codewars.js
@@ -3009,3 +3009,21 @@ function logicalCalc(array, op){
       throw new Error("Unknown operator");
   }
 }
+
+// You are given a string containing a sequence of character sequences separated by commas.
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+// Solution
+function array(string) {
+  if (!string) return null;
+  const arr = string.split(",");
+  if (arr.length <= 2) return null;
+  return arr.slice(1, -1).join(" ");
+}
