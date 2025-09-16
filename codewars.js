ed9d3e078bb08ba0e38b5ccafa4638c594a3310e
Eduard <eduard.marharian@gmail.com>
2025-09-16 12:05:56 +0300

    Duck Duck Goose

--- codewars.js
+++ codewars.js
@@ -3033,3 +3033,17 @@ function array(string) {
 function mergeArrays(a, b) {
   return [...new Set([...a, ...b].sort((a, b) => a - b))]
 }
+
+// Given an array of Player objects and a position (first position is 1), return the name of the chosen Player.
+// name is a property of Player objects, e.g Player.name
+// Example:
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+// Random input limits:
+// 6 ≤ Players ≤ 50
+// 5000 ≤ Position ≤ 50000
+// Solution
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name;
+}
