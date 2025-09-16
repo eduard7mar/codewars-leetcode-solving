a5444e29c3e2e72b965122499f6ee7fcda2c9123
Eduard <eduard.marharian@gmail.com>
2025-09-16 17:25:28 +0300

    Is there a vowel in there?

--- codewars.js
+++ codewars.js
@@ -3047,3 +3047,15 @@ function mergeArrays(a, b) {
 function duckDuckGoose(players, goose) {
   return players[(goose - 1) % players.length].name;
 }
+
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+// If they are, change the array value to a string of that vowel.
+// input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
+// Solution
+function isVow(a) {
+  return a.map(num => {
+    const char = String.fromCharCode(num);
+    return "aeiou".includes(char) ? char : num;
+  });
+}
+
