159f8767e64885ae509e8dcd0d608a003b9eb288
Eduard <eduard.marharian@gmail.com>
2025-09-22 18:23:05 +0300

    SpeedCode #2 - Array Madness

--- codewars.js
+++ codewars.js
@@ -3111,3 +3111,13 @@ const arrowFunc = function(arr) {
   return arr.map(a => String.fromCharCode(a)).join('');
 }
 
+// SpeedCode #2 - Array Madness
+// Objective
+// Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element
+// in a is strictly greater than the sum of the cubes of each element in b.
+// E.g.
+// arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+// Solution
+function arrayMadness(a, b) {
+  return a.reduce((el1, el2) => el1 + el2 * el2, 0) > b.reduce((el1, el2) => el1 + el2 * el2 * el2, 0);
+}
