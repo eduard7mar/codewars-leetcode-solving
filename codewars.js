81eeb8cacb06d9f2be3670748c21327c9afb8d8c
Eduard <eduard.marharian@gmail.com>
2025-09-23 20:44:34 +0300

    an array of objects

--- codewars.js
+++ codewars.js
@@ -3121,3 +3121,22 @@ const arrowFunc = function(arr) {
 function arrayMadness(a, b) {
   return a.reduce((el1, el2) => el1 + el2 * el2, 0) > b.reduce((el1, el2) => el1 + el2 * el2 * el2, 0);
 }
+
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+// Your task is to return the number of JavaScript developers coming from Europe.
+// For example, given the following list:
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+// If, there are no JavaScript developers from Europe then your function should return 0.
+// Solution
+function countDevelopers(list) {
+  let newArray = list.filter(
+    (item) => item.continent === "Europe" && item.language === "JavaScript"
+  );
+  return newArray.length > 0 ? newArray.length : 0;
+}
