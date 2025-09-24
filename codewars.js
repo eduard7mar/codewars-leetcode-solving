f8ad9285ffd0297d0d79fadf819549d2c5899d1c
Eduard <eduard.marharian@gmail.com>
2025-09-24 18:42:31 +0300

    pick a set of first elements

--- codewars.js
+++ codewars.js
@@ -3154,3 +3154,15 @@ function findAverage(nums) {
   return nums.reduce((a, b) => (a + b), 0) / nums.length;
 }
 
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+// If n == 0 return an empty sequence []
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+// Solution
+function first(arr, n = 1) {
+  return arr.slice(0, n);
+}
