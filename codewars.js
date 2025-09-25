37c5d56caf4208f896e508c1c5dc31dcc7f6cf88
Eduard <eduard.marharian@gmail.com>
2025-09-25 21:26:38 +0300

    Make a function add

--- codewars.js
+++ codewars.js
@@ -3166,3 +3166,16 @@ function findAverage(nums) {
 function first(arr, n = 1) {
   return arr.slice(0, n);
 }
+
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+// For example:
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+// Solution
+function add(arr) {
+  let sum = 0;
+  return arr.map((num) => {
+    sum += num;
+    return sum;
+  });
+}
