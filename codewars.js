b1b7eba3d8f0147c0f778a02044c69e585fb0a13
Eduard <eduard.marharian@gmail.com>
2025-09-28 22:46:53 +0300

    all vowels removed

--- codewars.js
+++ codewars.js
@@ -3211,3 +3211,20 @@ function squareUp(n) {
     for (let j = n; j >= 1; j--) res.push(j <= i ? j : 0);
   return res;
 }
+
+// This Kata is intended as a small challenge for my students
+// Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+// Example (Input --> Output)
+// "drake" --> "drk"
+// "aeiou" --> ""
+// remove_vowels("drake") // => "drk"
+// remove_vowels("aeiou") // => ""
+// Solution
+var removeVowels = function (str) {
+  const pattern = /[aeiou]/gi;
+  let result = str
+    .split("")
+    .map((item) => (item.match(pattern) ? "" : item))
+    .join("");
+  return result;
+};
