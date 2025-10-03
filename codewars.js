bfa27f2942b5f57d5899d61fea4cf2e716cde654
Eduard <eduard.marharian@gmail.com>
2025-10-03 23:46:05 +0300

    a sequence of integers

--- codewars.js
+++ codewars.js
@@ -3325,3 +3325,23 @@ function monkeyCount(n) {
   }
   return array;
 }
+
+// Your task is to write a function which returns the sum of a sequence of integers.
+// The sequence is defined by 3 non-negative values: begin, end, step.
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+// Examples
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+// Solution
+const sequenceSumm = (begin, end, step) => {
+  if (begin > end) {
+    return 0;
+  }
+  let array = [];
+  for (let i = begin; i <= end; i += step) {
+    array.push(i);
+  }
+  return array.reduce((a, b) => a + b);
+};
