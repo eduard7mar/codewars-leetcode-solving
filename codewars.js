1c28f810221d4e3be98d88bd6a5c38f655905f69
Eduard <eduard.marharian@gmail.com>
2025-10-08 15:56:03 +0300

    String cleaning

--- codewars.js
+++ codewars.js
@@ -3413,7 +3413,7 @@ function squareOrSquareRoot(array) {
 //  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
 // Your function would return the following array:
 // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
-// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. 
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed.
 // Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 // Solution
 function gooseFilter (birds) {
@@ -3421,3 +3421,24 @@ function gooseFilter (birds) {
   return birds.filter(bird => !geese.includes(bird));
 };
 
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database.
+// At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers.
+// Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+// Solution
+function stringClean(s){
+  const digits = ['0','1','2','3','4','5','6','7','8','9'];
+  return s.split('').filter(item => !digits.includes(item)).join('');
+}
+
+function stringClean(s){
+  return s.replace(/\d/g, "");
+}
+
+function stringClean(s){
+  return s.replace(/[0-9]/g, '');
+}
