90e9d9f69883a7ffcb495635a205e9d50d5d687b
Eduard <eduard.marharian@gmail.com>
2025-11-24 12:28:27 +0200

    Name Shuffler

--- codewars.js
+++ codewars.js
@@ -3624,3 +3624,11 @@ function well(x) {
       return 'I smell a series!'
   }
 }
+
+// Write a function that returns a string in which firstname is swapped with last name.
+// Example(Input --> Output)
+// "john McClane" --> "McClane john"
+// Solution
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
