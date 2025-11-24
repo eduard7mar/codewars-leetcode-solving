22a08e669e66dc89ebebed792d5cb7cd92058b8e
Eduard <eduard.marharian@gmail.com>
2025-11-24 11:48:06 +0200

    Well of Ideas - Easy Version

--- codewars.js
+++ codewars.js
@@ -3595,4 +3595,32 @@ function find_difference(a, b) {
   return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
 }
 
+// For every good kata idea there seem to be quite a few bad ones!
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'.
+// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'.
+// If there are no good ideas, as is often the case, return 'Fail!'.
+// Solution
+function well(x){
+  let good = [];
+  let bad = [];
+  x.map(item => item === "good" ? good.push(item) : bad.push(item));
+  return (good.length === 1 || good.length === 2) ?  "Publish!" : good.length > 2 ? "I smell a series!" : "Fail!";
+}
 
+const well = x => {
+  const good_count = x.filter(x => x == 'good').length;
+  return good_count < 1 ? 'Fail!' :
+         good_count < 3 ? 'Publish!' : 'I smell a series!';
+}
+
+function well(x) {
+  switch (x.filter(i => i === 'good').length) {
+    case 0:
+      return 'Fail!'
+    case 1:
+    case 2:
+      return 'Publish!'
+    default:
+      return 'I smell a series!'
+  }
+}
